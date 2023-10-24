1f62d260208a85f904080391ada6c4655df784cb
pm72 <46081859+pm72@users.noreply.github.com>
2023-10-24 13:51:03 +0400

    pm72

--- portfolio_project/personal_portfolio/blog/views.py
+++ portfolio_project/personal_portfolio/blog/views.py
@@ -1,4 +1,10 @@
 from django.shortcuts import render
+from .models import Blog
 
 def all_blogs(request):
-  return render(request, 'blog/all_blog.html')
+  blogs = Blog.objects.order_by('-date')[:5]
+
+  return render(request, 'blog/all_blog.html',
+                {
+                  'blogs': blogs
+                })
